bbc43e1460564f66f14ae551be04c02fca3c3aac
Tanya Sandoval <sandoval.tanya2@gmail.com>
2016-11-30 18:40:42 +0000

    Fixed typo in db.py

--- db.py
+++ db.py
@@ -1,5 +1,21 @@
 import sqlite3 as sqlite
 
+
 def connect_db(filename='app/db.sqlite'):
     conn = sqlite.connect(database=filename)
     return conn
+
+
+def init_tables(conn):
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS
+        ftdb
+        (
+        id INTEGER PRIMARY KEY,
+        datetime DATETIME,
+        text TEXT
+        )
+    """)
+    conn.commit()
+    pass
