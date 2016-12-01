9c1c85790446ab0289bbdd9d7d7f0b4ecda4306c
Tanya Sandoval <sandoval.tanya2@gmail.com>
2016-12-01 15:35:36 +0000

    Show database rows in app homepage.

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
 from flask import render_template
 from app import app
 
+from app.db import connect_db
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Tanya'}
-    return render_template('index.html', title='Home', user=user)
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT
+            *
+        FROM
+            ftdb
+    """)
+    rows = cur.fetchall()
+    return render_template('index.html', title='Home', user=user, rows=rows)
 
 app.run(debug=True)
